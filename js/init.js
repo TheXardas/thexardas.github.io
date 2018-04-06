e39dbdf94a40009bf5990014888f60273e2b1376
Tony G <TheXardas@gmail.com>
2018-04-06 16:22:17 +0300

    Updated carousel

--- js/init.js
+++ js/init.js
@@ -1,7 +1,14 @@
 (function($){
   $(function(){
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+    setTimeout(function() {
+      $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        dotsEach: 1,
+      });
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+    }, 1000);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
